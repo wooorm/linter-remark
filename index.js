e7f8efb91df19c1dd456ec8ae343783ec896c960
Jonathan Haines <jonno.haines@gmail.com>
2018-01-20 12:20:49 +1100

    clear idle callbacks and optionally loadDeps

--- index.js
+++ index.js
@@ -6,35 +6,40 @@
 /* Dependencies. */
 var CompositeDisposable = require('atom').CompositeDisposable;
 
+var engine;
 var idleCallbacks = new Set();
 
 /* Subscriptions. */
 var subscriptions = new CompositeDisposable();
 var config = {};
-var engine;
 
 /* Expose. */
 exports.activate = activate;
 exports.deactivate = deactivate;
 exports.provideLinter = linter;
 
-const makeIdleCallback = work => {
-  let callbackId;
+function loadDeps() {
+  if (!engine) {
+    engine = require('unified-engine-atom');
+  }
+}
+
+function makeIdleCallback(work) {
+  var callbackId;
   var callBack = () => {
     idleCallbacks.delete(callbackId);
     work();
   };
   callbackId = window.requestIdleCallback(callBack);
   idleCallbacks.add(callbackId);
-};
+}
 
 function scheduleIdleTasks() {
-  var linterRemarkInstallPeerPackages = () => {
+  function linterRemarkInstallPeerPackages() {
     require('atom-package-deps').install('linter-remark');
-  };
-  const linterRemarkLoadDependencies = () => {
-    engine = require('unified-engine-atom');
-  };
+  }
+
+  var linterRemarkLoadDependencies = loadDeps;
 
   if (!atom.inSpecMode()) {
     makeIdleCallback(linterRemarkInstallPeerPackages);
@@ -59,6 +64,8 @@ function activate() {
 
 /* Deactivation tasks. */
 function deactivate() {
+  idleCallbacks.forEach(callbackID => window.cancelIdleCallback(callbackID));
+  idleCallbacks.clear();
   subscriptions.dispose();
 }
 
@@ -75,6 +82,8 @@ function linter() {
 
 /* One run. */
 function lint(editor) {
+  loadDeps();
+
   return engine({
     processor: require('remark'),
     pluginPrefix: 'remark',
